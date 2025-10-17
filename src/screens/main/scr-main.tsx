99bedc973f610dbcb5206d95638e2c5b57dfc706
Edna Gudi <fimetrabajos@gmail.com>
2025-10-17 14:07:51 -0600

    Agregue la pantalla de notificaciones

--- src/screens/main/scr-main.tsx
+++ src/screens/main/scr-main.tsx
@@ -1,9 +1,13 @@
 // import Image from 'next/image';
+"use client";
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import styles from "./scr-main.module.css";
 import { SubScrMainMenu } from "./subscreens/subscr-main-menu";
 
 export function ScrMain() {
+  const router = useRouter();
   return(
     <div className={styles.mainLayout}>
       <div className={styles.topBar}>
@@ -21,7 +25,8 @@ export function ScrMain() {
         <div className={styles.bottomBarElement}>
           <Image src="/plus.svg" width={30} height={30} alt="Add" />
         </div>
-        <div className={styles.bottomBarElement}>
+        <div className={styles.bottomBarElement}
+             onClick={() => router.push("/notifications")}>
           <Image src="/bell.svg" width={30} height={30} alt="Notifications" />
         </div>
       </div>
